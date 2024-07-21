14d0d760c60736c5f448717108308058b35049ea
Ayush Thakur <ayushthakur667@gmail.com>
2024-07-21 02:29:20 -0400

    Clean up

--- src/main/java/com/project/AppointmentTab.java
+++ src/main/java/com/project/AppointmentTab.java
@@ -1,6 +1,5 @@
 package com.project;
 
-import java.util.Date;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
--- src/main/java/com/project/forms/FormPatient.java
+++ src/main/java/com/project/forms/FormPatient.java
@@ -15,7 +15,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
